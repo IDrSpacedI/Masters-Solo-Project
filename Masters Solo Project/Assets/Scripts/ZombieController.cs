e0490c19c1bbb63ca0fe72e0eb52ce8ebaa96252
IDrSpacedI <hi189@student.anglia.ac.uk>
2022-10-15 19:03:59 +0100

    enemy / animation

---
+++ Masters Solo Project/Assets/Scripts/ZombieController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieController : MonoBehaviour
+{
+    private NavMeshAgent agent = null;
+    private Animator anim = null;
+    [SerializeField] private Transform target;
+    [SerializeField] private float stoppingDistance;
+
+    private void Start()
+    {
+        GetReference();
+    }
+
+    private void Update()
+    {
+        MoveToTarget();
+    }
+
+    private void MoveToTarget()
+    {
+        agent.SetDestination(target.position);
+        anim.SetFloat("Speed", 1f, 0.3f, Time.deltaTime);
+        RotateToTarget();
+
+        //float distanceToTaget = Vector3.Distance(transform.position, target.position);
+
+        //if(distanceToTaget <= stoppingDistance)
+        //{
+        //    RotateToTarget();
+        //}
+    }
+
+    private void RotateToTarget()
+    {
+        transform.LookAt(target);
+
+        Vector3 direction = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = rotation;
+    }
+
+    private void GetReference()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+
+    }
+}
