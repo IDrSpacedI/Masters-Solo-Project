1c4875cb0b6d848ba9138abc86f66525260e883a
IDrSpacedI <hi189@student.anglia.ac.uk>
2022-10-16 15:46:06 +0100

    character / player stats

---
+++ Masters Solo Project/Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    [SerializeField] protected int health;
+    [SerializeField] protected int maxHealth;
+
+    [SerializeField] protected bool isDead;
+
+    private void Start()
+    {
+        InitVaribles();
+    }
+
+    public void CheckHealth()
+    {
+        if(health <= 0)
+        {
+            health = 0;
+            isDead = true;
+        }
+        if(health >= maxHealth)
+        {
+            health = maxHealth;
+        }
+    }
+
+    public void Die()
+    {
+        isDead = true;
+    }
+
+    private void SetHealthTo(int healthToSetTo)
+    {
+        health = healthToSetTo;
+        CheckHealth();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        int healthAfterDamage = health - damage;
+        SetHealthTo(healthAfterDamage);
+    }
+
+    public void Heal(int heal)
+    {
+        int healAfterHeal = health + heal;
+        SetHealthTo(healAfterHeal);
+    }
+
+    public void InitVaribles()
+    {
+        maxHealth = 100;
+        SetHealthTo(maxHealth);
+        isDead = false;
+
+    }
+
+
+}
