a05593fabca157a53ebd3f0cfb960acca3c9c820
IDrSpacedI <hi189@student.anglia.ac.uk>
2022-11-01 16:20:53 +0000

    weapon shoot

--- Masters Solo Project/Assets/Scripts/EquipmentManager.cs
+++ Masters Solo Project/Assets/Scripts/EquipmentManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EquipmentManager : MonoBehaviour
 {
+    public int currentlyEquipedWeapon = 0;
+    private GameObject currentWeaponObject = null;
+
     [SerializeField] private Transform weaponHolder = null;
 
     private Inventory inventory;
@@ -15,26 +18,47 @@ public class EquipmentManager : MonoBehaviour
 
     public void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Alpha1))
+        //if (Input.GetKeyDown(KeyCode.Alpha1))
+        //{
+        //    setWeaponAnimation(0, WeaponType.Shotgun);
+        //    InstantiateWeapon(inventory.GetItem(0).prefab, 0);
+        //}
+
+
+        //weapon slot 1
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            setWeaponAnimation(0, WeaponType.Shotgun);
-            EquipWeapon(inventory.GetItem(0).prefab, 0);
+            UnequipWeapon();
+            EquipWeapon(inventory.GetItem(0));
         }
+
+
     }
 
-    private void setWeaponAnimation(int weaponStyle, WeaponType weaponType)
-    {
-        Weapon weapon = inventory.GetItem(weaponStyle);
-    }
+    //private void setWeaponAnimation(int weaponStyle, WeaponType weaponType)
+    //{
+    //    Weapon weapon = inventory.GetItem(weaponStyle);
+    //}
 
-    private void EquipWeapon(GameObject weaponObject, int weaponStyle)
-    {
-        Weapon weapon = inventory.GetItem(weaponStyle);
-        if(weapon != null)
-        {
-            Instantiate(weaponObject, weaponHolder);
-        }
+    //private void InstantiateWeapon(GameObject weaponObject, int weaponStyle)
+    //{
+    //    Weapon weapon = inventory.GetItem(weaponStyle);
+    //    if(weapon != null)
+    //    {
+    //        currentWeaponObject = Instantiate(weaponObject, weaponHolder);
+    //    }
         
+    //}
+
+    private void EquipWeapon(Weapon weapon)
+    {
+        currentlyEquipedWeapon = (int)weapon.WeaponStyle;
+        Instantiate(weapon.prefab, weaponHolder);
+    }
+
+    private void UnequipWeapon()
+    {
+        Destroy(currentWeaponObject);
     }
 
     private void getReferences()
--- Masters Solo Project/Assets/Scripts/WeaponShoot.cs
+++ Masters Solo Project/Assets/Scripts/WeaponShoot.cs
@@ -6,6 +6,8 @@ public class WeaponShoot : MonoBehaviour
 {
 
     private Camera cam;
+    private Inventory inventory;
+    private EquipmentManager manager;
     
 
 
@@ -19,7 +21,8 @@ public class WeaponShoot : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
-            //shoot
+            Debug.Log("pew pew");
+            Shoot();
         }
     }
 
@@ -28,11 +31,20 @@ public class WeaponShoot : MonoBehaviour
         Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
         RaycastHit hit;
 
+        float currentWeaponRange = inventory.GetItem(manager.currentlyEquipedWeapon).range;
+
+        if (Physics.Raycast(ray, out hit, currentWeaponRange))
+        {
+            Debug.Log(hit.transform.name);
+        }
+
        
     }
 
     private void GetReferences()
     {
         cam = GetComponentInChildren<Camera>();
+        inventory = GetComponent<Inventory>();
+        manager = GetComponent<EquipmentManager>();
     }
 }
